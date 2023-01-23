f27e4849b7fb912b4159f8b27abba5096b96f236
Patrick Morpey <patrick@morphp.nl>
2023-01-23 12:28:07 +0100

    RE

--- src/ConfigOptionResolver.php
+++ src/ConfigOptionResolver.php
@@ -24,11 +24,10 @@ class ConfigOptionResolver implements ParameterResolver
         $optionController = service(OptionController::class);
 
         if (!$optionController->hasValue($option)) {
-            // We throw an exception, because it should be expected that,
-            // if the ConfigValue attribute is used, that the value actually
-            // be defined in the defaults or the config, instead of
-            // skipping to the next ParameterResolver
-            throw new Exceptions\NoConfigValueFound($option);
+            // We don't throw an exception, because the parameter
+            // could be nullable, which means an unset config value
+            // is allowed
+            return false;
         }
 
         $this->result = $optionController->getValue($option);
