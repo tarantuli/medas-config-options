d14aee54197f8ecd3e9373789eccad286cd4bf6d
Patrick Morpey <patrick@morphp.nl>
2023-11-20 14:36:17 +0100

    Reformatting

--- src/GlobalFunctions.php
+++ src/GlobalFunctions.php
@@ -2,11 +2,10 @@
 
 declare(strict_types=1);
 
-// This file should be in the global namespace
-
 use Medas\ConfigOptions\OptionController;
 use Medas\Core\Interfaces\ConfigOption;
 
+// This file should be in the global namespace
 function option(ConfigOption $option): mixed
 {
     return service(OptionController::class)->getValue($option);
