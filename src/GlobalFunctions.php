2667c3483575dc239130a3f0c17ebaba942e6bf3
Patrick Morpey <patrick@morphp.nl>
2026-02-23 11:38:28 +0100

    Guard against function definitions

--- src/GlobalFunctions.php
+++ src/GlobalFunctions.php
@@ -6,7 +6,9 @@ use Medas\ConfigOptions\OptionController;
 use Medas\Core\Interfaces\ConfigOption;
 
 // This file should be in the global namespace
-function option(ConfigOption $option): mixed
-{
-    return service(OptionController::class)->getValue($option);
+if (!function_exists('option')) {
+    function option(ConfigOption $option): mixed
+    {
+        return service(OptionController::class)->getValue($option);
+    }
 }
